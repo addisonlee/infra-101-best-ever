d6856607b53070a489beb91c64ed3a2ccd32c1ed
Addison Lee <addison.v.lee@gmail.com>
2020-05-09 20:23:29 -0400

    Remove code which would not have passed a PR

--- routes/greeting.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    checkFunction: function(req, res) {
-        if (!req.body.name) {
-            res.send('Error: "name" is a required parameter');
-            return;
-        }
-        res.send('Greetings: ' + req.body.name);
-    }
-};
